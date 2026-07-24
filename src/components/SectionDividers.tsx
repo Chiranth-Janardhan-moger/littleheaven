6694bf639187d660a14466006b1cc59387ebdea0
Chiranth-Janardhan-moger <chiranthmoger7@gmail.com>
2026-07-24 23:34:52 +0530

    style(dividers): rotate footImg 90 degrees to point toes/fingers rightward

--- src/components/SectionDividers.tsx
+++ src/components/SectionDividers.tsx
@@ -145,7 +145,7 @@ export const MissionToProgramsDivider: React.FC = () => {
                 src={footImg}
                 alt="Footprint"
                 className="w-8 h-8 sm:w-10 sm:h-10 object-contain text-blue-600"
-                style={{ transform: step.isRight ? 'scaleY(-1)' : 'none' }}
+                style={{ transform: `rotate(90deg) ${step.isRight ? 'scaleY(-1)' : ''}` }}
               />
             ) : (
               <ToddlerFootprint isRightFoot={step.isRight} className="w-8 h-6 sm:w-9 sm:h-7 text-blue-600" />
